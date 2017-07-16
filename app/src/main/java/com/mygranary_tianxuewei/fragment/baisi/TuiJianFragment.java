40fc27ac6cdfee2354bc046b108d29fe5ac0c3c9
tianxuewei1016 <93226539@qq.com>
2017-07-16 13:21:36 +0800

    百思不得姐1

--- app/src/main/java/com/mygranary_tianxuewei/fragment/baisi/TuiJianFragment.java
+++ app/src/main/java/com/mygranary_tianxuewei/fragment/baisi/TuiJianFragment.java
@@ -1,7 +1,17 @@
 package com.mygranary_tianxuewei.fragment.baisi;
 
+import android.support.v7.widget.RecyclerView;
+import android.widget.ProgressBar;
+
+import com.jwenfeng.library.pulltorefresh.PullToRefreshLayout;
 import com.mygranary_tianxuewei.R;
+import com.mygranary_tianxuewei.adapter.TuiJianAdapter;
 import com.mygranary_tianxuewei.base.BaseFragment;
+import com.mygranary_tianxuewei.bean.TuiJianBean;
+
+import java.util.List;
+
+import butterknife.Bind;
 
 /**
  * 作者：田学伟 on 2017/7/15 18:13
@@ -10,6 +20,16 @@ import com.mygranary_tianxuewei.base.BaseFragment;
  */
 
 public class TuiJianFragment extends BaseFragment {
+
+
+    @Bind(R.id.rl_share_recommend)
+    RecyclerView rlShareRecommend;
+    @Bind(R.id.refresh)
+    PullToRefreshLayout refresh;
+    @Bind(R.id.progressbar)
+    ProgressBar progressbar;
+    private List<TuiJianBean.ListBean> list;
+    private TuiJianAdapter adapter;
 
     @Override
     protected int getLayoutId() {
